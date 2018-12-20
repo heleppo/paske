56e88e0835a14fe2bf944e840b99e0ad8551fa4c
heleppo <ilenopanen@gmail.com>
2018-12-21 01:56:44 +0200

    koira

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+
+
+using namespace std;
+
+int main() {
+    unsigned long k;
+    string str="RISTUKSEN VITTU MOON PÖHÖNÄS!"
+               "HAISTA KOIRA VITTU!";
+    for (k = 0; k < str.size(); ++k) {
+        if (k < 31) {
+            cout << str[k];
+        } else {
+            cout << str[k] << endl;
+            }
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
