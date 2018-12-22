535216df18af1848b9b902aa595ff2de9fb5d629
jykä <jyrki@sposti.com>
2018-12-22 22:33:39 +0200

    nonnäi

--- student/01/eka_palautus/main.cpp
+++ student/01/eka_palautus/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::string name = "";
+    string name = "";
     int age = 0;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    std::cout << "Pleased to meet you, "
-              << name << "!" << std::endl;
-    std::cout << "In your next birthday you'll be "
-              << age + 1 << " old!" << std::endl;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Pleased to meet you, "
+              << name << "!" << endl;
+    cout << "In your next birthday you'll be "
+              << age + 1 << " old!" << endl;
 }
