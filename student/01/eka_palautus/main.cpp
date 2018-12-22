2bce4d014b05fb96d5a59b4ff6a876d3b5f28c1e
heleppo <ilenopanen@gmail.com>
2018-12-22 22:24:58 +0200

    stna

--- student/01/eka_palautus/main.cpp
+++ student/01/eka_palautus/main.cpp
@@ -5,9 +5,9 @@ int main() {
     int age = 0;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    std::cout << std::endl << "Enter your age: ";
+    std::cout << "Enter your age: ";
     std::cin >> age;
-    std::cout << std::endl << "Pleased to meet you, "
+    std::cout << "Pleased to meet you, "
               << name << "!" << std::endl;
     std::cout << "In your next birthday you'll be "
               << age + 1 << " old!" << std::endl;
