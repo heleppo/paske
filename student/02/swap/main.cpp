addf92fa38ca97caba58db0880ee2ee43816b960
heleppo <ilenopanen@gmail.com>
2018-12-23 22:51:46 +0200

    hmmmmmmmmmmmm

---
+++ student/02/swap/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int swap(int num1, int num2) {
+    return num1;
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Enter an integer: " << endl;
+    cin >> a;
+    cout << "Enter another integer: " << endl;
+    cin >> b;
+}
