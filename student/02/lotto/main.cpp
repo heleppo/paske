a4beee958f6a1515c11b74dc9d115d5e6db303cd
heleppo <ilenopanen@gmail.com>
2018-12-23 19:48:39 +0200

    factorial long long

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -3,24 +3,32 @@
 
 using namespace std;
 
-int factorial(int num) {
+long long int factorial(int num) {
     if ( num == 0 ) {
         return 1;
     } else {
-        int f = num;
-        for ( int a = 1; a < num; a++) {
+        long long int f = num;
+        for (int a = 2; a < num; a++) {
             f *= a;
         }
         return f;
     }
 }
 
-
-int chance(int n, int p) {
-    return 0;
+long long int chance(int n, int p) {
+    return factorial(n) / (factorial(n - p) *
+                           factorial(p));
 }
 
-int main()
-{
-
+int main() {
+//    int total = 0;
+//    int drawn = 0;
+//    cout << "Enter the total number of lottery balls: ";
+//    cin >> total;
+//    cout << "Enter the number of the drawn balls: ";
+//    cin >> drawn;
+//    cout << "The probability of guessing all 4 balls "
+//            "correctly is 1/" << chance(total, drawn)
+//         << endl;
+    cout << factorial(20);
 }
