ad00d0a1c8b2f4b398bf6e0b5de4fc3ff0dfe5f2
heleppo <ilenopanen@gmail.com>
2018-12-24 02:51:05 +0200

    int&

--- student/02/swap/main.cpp
+++ student/02/swap/main.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
-int swap(int num1, int num2) {
-    return num1;
+void swap(int& num1, int& num2) {
+    int num1_temp = num1;
+    num1 = num2;
+    num2 = num1_temp;
 }
 
 int main()
 {
     int a = 0;
     int b = 0;
-    cout << "Enter an integer: " << endl;
+    cout << "Enter an integer: ";
     cin >> a;
-    cout << "Enter another integer: " << endl;
+    cout << "Enter another integer: ";
     cin >> b;
+    swap(a, b);
+    cout << "The integers are " << a <<
+            " and " << b << endl;
 }
