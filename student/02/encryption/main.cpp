e3c857b01eded48e5a4ef92e6f2edbfb4dc67d9f
heleppo <ilenopanen@gmail.com>
2018-12-24 22:30:41 +0200

    a-z kaikki v2

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -5,22 +5,26 @@
 using namespace std;
 
 int checkaz(string key) {
-    deque<char> az = {};
-    for (char i = 97; i < 123; ++i) {
-        az.push_back(i);
-    }
     for (string::size_type a = 0;
          a < key.length(); ++a) {
         if (not islower(key[a])) {
             return 1;
         } else if (key[a] < 97 or key[a] > 122) {
             return 1;
-        } else if (key.find(az[a]) ==
-                   string::npos) {
-            cout << "Error! The encryption key "
-                    "must contain all alphabets "
-                    "a-z." << endl;
-            return 0;
+        }
+    }
+    return 0;
+}
+
+int checkall(string key) {
+    deque<char> az = {};
+    for (char i = 97; i < 123; ++i) {
+        az.push_back(i);
+    }
+    for (string::size_type a = 0;
+         a < key.length(); ++a) {
+        if (key.find(az[a]) == string::npos) {
+            return 1;
         }
     }
     return 0;
@@ -38,6 +42,11 @@ int main() {
         cout << "Error! The encryption key must "
                 "contain only lower case characters."
              << endl;
+        return 1;
+    } else if (checkall(key)) {
+        cout << "Error! The encryption key "
+                "must contain all alphabets "
+                "a-z." << endl;
         return 1;
     }
 }
