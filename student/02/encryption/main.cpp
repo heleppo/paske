fd85c779e8cd29d1708b5e7820f920d86648f9a3
heleppo <ilenopanen@gmail.com>
2018-12-24 22:18:10 +0200

    a-z kaikki kiri aimet

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
+#include <deque>
 
 using namespace std;
 
 int checkaz(string key) {
+    deque<char> az = {};
+    for (char i = 97; i < 123; ++i) {
+        az.push_back(i);
+    }
     for (string::size_type a = 0;
          a < key.length(); ++a) {
         if (not islower(key[a])) {
             return 1;
         } else if (key[a] < 97 or key[a] > 122) {
             return 1;
+        } else if (key.find(az[a]) ==
+                   string::npos) {
+            cout << "Error! The encryption key "
+                    "must contain all alphabets "
+                    "a-z." << endl;
+            return 0;
         }
     }
     return 0;
